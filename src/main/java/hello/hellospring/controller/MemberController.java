ad7f81780a0758c9e25562ac6e1e1c2b34eab6a7
GIGEON.MUN <ggmun@lfit.co.kr>
2022-07-13 16:47:11 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,18 @@
+package hello.hellospring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+
+}
