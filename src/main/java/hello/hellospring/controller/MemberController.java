90fe2002ebb0e497847d1544c705c7cf37f56d31
GIGEON.MUN <ggmun@lfit.co.kr>
2022-07-15 13:34:26 +0900

    1

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
